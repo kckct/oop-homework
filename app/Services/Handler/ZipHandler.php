e7066392e476d78c1f738f755ad4f2d62201091f
kckct <ss010359@gmail.com>
2017-11-01 02:41:26 +0800

    加入 ZipHandler

---
+++ app/Services/Handler/ZipHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services\Handler;
+
+use App\Services\Candidate;
+
+/**
+ * Class ZipHandler
+ * @package App\Services\Handler
+ */
+class ZipHandler extends AbstractHandler
+{
+    /**
+     * 處理檔案
+     * @param Candidate $candidate 描述待處理檔案的資訊
+     * @param array $target byte[]
+     * @return array
+     */
+    public function perform(Candidate $candidate, array $target): array
+    {
+        if (empty($target)) {
+            return [];
+        }
+
+        return $this->zipData($candidate, $target);
+    }
+
+    /**
+     * 壓縮
+     * @param Candidate $candidate
+     * @param array $target
+     * @return array
+     */
+    private function zipData(Candidate $candidate, array $target): array
+    {
+        return collect($target)->map(function ($item) {
+            return gzcompress((string)$item);
+        })->toArray();
+    }
+}
