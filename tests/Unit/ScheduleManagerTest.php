c1255c9931fe3a20f45cbb8791cbac364441ade9
kckct <ss010359@gmail.com>
2017-10-31 22:57:10 +0800

    Schedule 測試補強

--- tests/Unit/ScheduleManagerTest.php
+++ tests/Unit/ScheduleManagerTest.php
@@ -24,23 +24,31 @@ class ScheduleManagerTest extends TestCase
 
     public function test_有schedules屬性()
     {
+        // assert
         $this->assertObjectHasAttribute('schedules', $this->scheduleManger);
     }
 
     public function test_讀取json檔有3筆schedule設定()
     {
+        // assert
         $this->assertEquals(3, $this->scheduleManger->count());
     }
 
     public function test_可取得schedules陣列且為Schedule物件()
     {
+        // act
         $schedules = $this->scheduleManger->getSchedules();
 
+        // assert
         // schedules 屬性為 array
         $this->assertTrue(is_array($schedules));
         // schedules array 皆為 Schedule 物件
         $this->assertInstanceOf(Schedule::class, $schedules[0]);
         $this->assertInstanceOf(Schedule::class, $schedules[1]);
         $this->assertInstanceOf(Schedule::class, $schedules[2]);
+        // schedules[0] 所有 property 值正確
+        $this->assertEquals('cs', $schedules[0]->getExt());
+        $this->assertEquals('Everyday', $schedules[0]->getInterval());
+        $this->assertEquals('12:00', $schedules[0]->getTime());
     }
 }
--- tests/Unit/ScheduleTest.php
+++ tests/Unit/ScheduleTest.php
@@ -16,18 +16,41 @@ class ScheduleTest extends TestCase
         parent::setUp();
     }
 
-    public function test_有預設屬性()
+    public function test_傳入空陣列有預設屬性()
     {
-        $inputStub = collect([
-            'ext'      => '',
-            'interval' => '',
-            'time'     => '',
-        ]);
+        // arrange
+        $inputStub = collect([]);
 
+        // act
         $config = new Schedule($inputStub);
 
+        // assert
+        // Schedule 有屬性
         $this->assertObjectHasAttribute('ext', $config);
         $this->assertObjectHasAttribute('interval', $config);
         $this->assertObjectHasAttribute('time', $config);
+        // 屬性值是否正確
+        $this->assertEquals('', $config->getExt());
+        $this->assertEquals('', $config->getInterval());
+        $this->assertEquals('', $config->getTime());
+    }
+
+    public function test_傳入陣列預設屬性值正確()
+    {
+        // arrange
+        $inputStub = collect([
+            'ext'      => 'php',
+            'interval' => 'Friday',
+            'time'     => '12:34',
+        ]);
+
+        // act
+        $config = new Schedule($inputStub);
+
+        // assert
+        // 屬性值是否正確
+        $this->assertEquals('php', $config->getExt());
+        $this->assertEquals('Friday', $config->getInterval());
+        $this->assertEquals('12:34', $config->getTime());
     }
 }
