afab4616c2b3c852fcbd13b346e94cb74a7ff161
kckct <ss010359@gmail.com>
2017-11-07 19:13:45 +0800

    加入 FileFinder interface

---
+++ app/Services/File/FileFinder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services\File;
+
+use ArrayAccess;
+use IteratorAggregate;
+
+/**
+ * Interface FileFinder
+ * @package App\Services\File
+ */
+interface FileFinder extends IteratorAggregate, ArrayAccess
+{
+    
+}
